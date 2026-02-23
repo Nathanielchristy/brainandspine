1d971269cc34c33757d887a6fdadfae9d0397470
varun1241 <varun124150@gmail.com>
2026-02-23 19:49:10 +0530

    header_updaetd

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import { Footer } from '@/components/footer'
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>
         <Hero />
         <About />
